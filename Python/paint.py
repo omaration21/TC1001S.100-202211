6a8ff8aa2de5c3a7f148f577926ceda50adb97da
luciabarre <lbarrenecheac@gmail.com>
2022-03-24 10:46:30 -0600

    add option to draw triangle

--- Python/paint.py
+++ Python/paint.py
@@ -40,7 +40,22 @@ def circle(start, end):
 def rectangle(start, end):
     "Draw rectangle from start to end."
     pass  # TODO
-    
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+
+    forward(end.x - start.x)
+    left(90)
+    forward((end.x - start.x)/2)
+    left(90)
+    forward(end.x - start.x)
+    left(90)
+    forward((end.x - start.x)/2)
+    left(90)
+
+    end_fill()
 def triangle(start, end):
     "Draw triangle from start to end."
     pass  # TODO
